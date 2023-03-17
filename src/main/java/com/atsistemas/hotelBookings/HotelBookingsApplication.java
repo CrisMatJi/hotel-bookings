8b2622d9d4576878f3435cec6e88cc2b66c02f77
crismatji <c.mateos.jimenez@gmail.com>
2023-03-17 12:48:12 +0100

    Primer commit , incluyendo ficheros SQL y base del proyecto

---
+++ src/main/java/com/atsistemas/hotelBookings/HotelBookingsApplication.java
@@ -0,0 +1,13 @@
+package com.atsistemas.hotelBookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atsistemas/hotelBookings/HotelBookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsistemas.hotelBookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
